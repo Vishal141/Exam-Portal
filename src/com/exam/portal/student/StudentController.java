86fe537ea61e40c3ab1dd9a6b08fb2efcbf92011
ABHISHEK <abhishek.ak1012@gmail.com>
2021-10-13 12:43:11 +0530

    UI of student dashboard complted

---
+++ src/com/exam/portal/student/StudentController.java
@@ -0,0 +1,17 @@
+package com.exam.portal.student;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class studentController {
+
+    @FXML
+    private Button btn;
+
+    @FXML
+    void enteredbutton(ActionEvent event) {
+
+    }
+
+}
