12181346d94d15d849699c1013dc8ba84cd7f6c4
naradmuni <naradmuni241124@gmail.com>
2021-10-13 11:43:24 +0530

    UI for Create Teams Added

---
+++ src/com/exam/portal/teams/CreateTeam.java
@@ -0,0 +1,20 @@
+package com.exam.portal.teams;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreateTeam {
+    @FXML
+    private TextField txtFieldTeamName;
+
+    @FXML
+    private Button createTeamBtn;
+
+    @FXML
+    void createTeam(ActionEvent event) {
+
+    }
+
+}
