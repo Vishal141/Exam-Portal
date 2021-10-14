d345436393c39537cfcebc88f8e7b835a21de81a
naradmuni <naradmuni241124@gmail.com>
2021-10-14 10:16:55 +0530

    method template and UI for add student to a team added

---
+++ src/com/exam/portal/teams/AddStudentController.java
@@ -0,0 +1,28 @@
+package com.exam.portal.teams;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class AddStudentController {
+
+    @FXML
+    private Label lblTeamName;
+
+    @FXML
+    private TextField searchBar;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private ListView<?> listViewStudent;
+
+    @FXML
+    void search(ActionEvent event) {
+
+    }
+}
