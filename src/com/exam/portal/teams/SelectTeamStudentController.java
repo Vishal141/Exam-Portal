c72d23ba7e847f6ec03bf1e71248c41b67daafeb
naradmuni <naradmuni241124@gmail.com>
2021-10-16 09:27:43 +0530

    method structure and UI for selected teams Added

---
+++ src/com/exam/portal/teams/SelectTeamStudentController.java
@@ -0,0 +1,48 @@
+package com.exam.portal.teams;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SelectTeamStudentController {
+    @FXML
+    private Label lblTeamName;
+
+    @FXML
+    private Button btnShowStudents;
+
+    @FXML
+    private Button btnSheduleExam;
+
+    @FXML
+    private Button btnGoBcak;
+
+    @FXML
+    private TextField tfMassage;
+
+    @FXML
+    private Button btnSend;
+
+    @FXML
+    void checkExam(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void send(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showAllStudents(ActionEvent event) {
+
+    }
+
+}
---
+++ src/com/exam/portal/teams/SelectTeamTeacherController.java
@@ -0,0 +1,58 @@
+package com.exam.portal.teams;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class SelectTeamTeacherController {
+
+
+    @FXML
+    private Label lblTeamName;
+
+    @FXML
+    private Button btnShowStudents;
+
+    @FXML
+    private Button btnAddStudent;
+
+    @FXML
+    private Button btnSheduleExam;
+
+    @FXML
+    private Button btnGoBcak;
+
+    @FXML
+    private TextField tfMassage;
+
+    @FXML
+    private Button btnSend;
+
+    @FXML
+    void addStudent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void send(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sheduleExam(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showAllStudents(ActionEvent event) {
+
+    }
+
+}
